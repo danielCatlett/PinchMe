23fc295af87b1ac2c0e7a8ed0d4abbb1cdb458b4
danielCatlett <daniel.catlett42@gmail.com>
2017-05-10 17:07:05 -0500

    Pinching

--- PinchMe/ViewController.swift
+++ PinchMe/ViewController.swift
@@ -1,25 +1,69 @@
-//
 //  ViewController.swift
 //  PinchMe
 //
 //  Created by Daniel Catlett on 5/10/17.
 //  Copyright © 2017 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UIGestureRecognizerDelegate
+{
+    private var imageView:UIImageView!
+    private var scale = CGFloat(1)
+    private var previousScale = CGFloat(1)
+    private var rotation = CGFloat(0)
+    private var previousRotation = CGFloat(0)
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "yosemite-meadows")
+        imageView = UIImageView(image: image)
+        imageView.isUserInteractionEnabled = true
+        imageView.center = view.center
+        view.addSubview(imageView)
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        pinchGesture.delegate = self
+        imageView.addGestureRecognizer(pinchGesture)
+        
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.doRotate(_:)))
+        rotationGesture.delegate = self
+        imageView.addGestureRecognizer(rotationGesture)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithotherGestureRecognizer: UIGestureRecognizer) -> Bool
+    {
+        return true
     }
-
-
+    
+    func transformImageView()
+    {
+        var t = CGAffineTransform(scaleX: scale * previousScale, y: scale * previousScale)
+        t = t.rotate(rotation + previousRotation)
+        imageView.transform = t
+    }
+    
+    func doPinch(_ gesture:UIPinchGestureRecognizer)
+    {
+        scale = gesture.scale
+        transformImageView()
+        if(gesture.state == .ended)
+        {
+            previousScale = scale * previousScale
+            scale = 1
+        }
+    }
+    
+    func doRotate(_ gesture:UIRotationGestureRecognizer)
+    {
+        rotation = gesture.rotation
+        transformImageView()
+        if(gesture.state == .ended)
+        {
+            previousRotation = rotation + previousRotation
+            rotation = 0
+        }
+    }
 }
-
